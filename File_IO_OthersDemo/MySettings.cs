e30c06d72332fd74901a4e55298ea98af3422dd8
zafar ali <zafaralipro110@gmail.com>
2017-05-04 21:09:19 +0500

    In MySettings class ,replaced the StreamWriter and StreamReader with the XmlSerializer.So that our app becomes more persistance and we no need to worry about the data type of our settings.

--- File_IO_OthersDemo/MySettings.cs
+++ File_IO_OthersDemo/MySettings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Xml.Serialization;
 /*
 whenever working with IO in dot net framework,always remember three classes... 1.Path 2. Directory and 3.File
 when working with file we have stream class but that stream class is works with bytes[] which is very low so in order to read and write Files we should
@@ -52,7 +53,8 @@ namespace File_IO_OthersDemo
         {
             get
             {
-                return Path.Combine(SettingsFolder, "settings.txt");
+             // return Path.Combine(SettingsFolder, "settings.txt");  //we no longer creating a txt file bcoz we are working with the XmlSerializer
+                return Path.Combine(SettingsFolder, "settings.xml");
             }
         }
 
@@ -61,14 +63,27 @@ namespace File_IO_OthersDemo
 
             using (Stream stream = File.Create(SettingsFile))   //creating a file named 'settings.txt'
             //since stream works on byte[] and we dont want to work at that low level so we can use StreamWriter on top of stream
-            using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8))
+            #region Old Logic(when we was working with txt file and the StreamWriter)
+            //using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8))
+            //{
+
+            //    writer.WriteLine(MyNumber);
+            //    writer.WriteLine(MyString);
+
+            //    //writer.Close();   //since we are using the USING statement so we dont need to close the stream explicitly
+            //    //stream.Close();
+            //}
+            #endregion
             {
+                // inorder to acheive persistance in our application we can replace StreamWriter with XmlSerializer 
+                // by doing this we no longer concern with the type of our data e.g int or string or etc...
+                // Imp Note :- XmlSerializer will serialize the PUBLIC properties of the Object...we can provide the type of the object
+                // and then the XmlSerializer uses Reflection to determine the meta data and types of all the public properties of the 
+                // object and then serialized them..
 
-                writer.WriteLine(MyNumber);
-                writer.WriteLine(MyString);
+                XmlSerializer ser = new XmlSerializer(this.GetType());
+                ser.Serialize(stream, this);
 
-                //writer.Close();   //since we are using the USING statement so we dont need to close the stream explicitly
-                //stream.Close();
             }
         }
 
@@ -78,34 +93,38 @@ namespace File_IO_OthersDemo
             {
                 return DefaultSettings;
             }
+
             using (Stream stream = File.OpenRead(SettingsFile))
-            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            #region OldLogic(when we was working with txt file and the StreamReader)        
+            //using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            //{
+            //    try
+            //    {
+            //        string firstLine = reader.ReadLine();  //reads the first line
+            //        string secondLine = reader.ReadLine();  //reads the second line
+
+            //        return new MySettings
+            //        {
+            //            MyNumber = int.Parse(firstLine), //we are parsing here and if the first line of file isnt int then this line will throw an exception that's why trycatch is used
+            //            MyString = secondLine
+
+            //        };
+            //    }
+            //    catch (FormatException)  //if file format is corrupt or the file is corrupt
+            //    {
+            //        stream.Close();
+            //        File.Delete(SettingsFile);  //delete the corrupt file
+
+            //        return DefaultSettings;
+            //    }
+
+            //}
+            #endregion
             {
-                try
-                {
-                    string firstLine = reader.ReadLine();  //reads the first line
-                    string secondLine = reader.ReadLine();  //reads the second line
-
-                    //reader.Close();
-                    //stream.Close();
-
-                    return new MySettings
-                    {
-                        MyNumber = int.Parse(firstLine), //we are parsing here and if the first line of file isnt int then this line will throw an exception that's why trycatch is used
-                        MyString = secondLine
-
-                    };
-                }
-                catch (FormatException)  //if file format is corrupt or the file is corrupt
-                {
-                    stream.Close();
-                    File.Delete(SettingsFile);  //delete the corrupt file
-
-                    return DefaultSettings;
-                }
-
+                
+                XmlSerializer ser = new XmlSerializer(typeof(MySettings));
+                return (MySettings)ser.Deserialize(stream);  //note : Xml Deserializer returns an object so need to cast it approperiatly
             }
-
         }
 
 
